22d88c639014c1cd8da8b192c77d01a3320a0968
Thyago Lucas <thyago.devin@gmail.com>
2023-01-10 11:32:49 -0400

    feat: pyramid of numbers delivery

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class App {
+
     public static void main(String[] args) throws Exception {
+
         System.out.println("Hello, World!");
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("\n digite um numero para formar uma pirâmide \n");
+
+        int number = reader.nextInt();
+
+        for(int i = 1 ; i<= number ; i++){
+            String output = "";
+            for(int j = 1 ; j <= i ; j++){
+                
+                output = output + " " + i;
+            }
+            System.out.println(output);
+        }
     }
 }
